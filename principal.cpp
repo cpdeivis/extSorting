396ce06fdbcf6bb75d065681a4e43b377f92dc54
Deivis Costa Pereira <20757773+oscaruno@users.noreply.github.com>
2018-05-10 10:03:05 -0300

    Update principal.cpp

--- principal.cpp
+++ principal.cpp
@@ -3,9 +3,11 @@
 
 int main(int argc, char **argv){
     try{
-        extSort ex{2,6};
-        ex.clfInterna("input.txt");
-        ex.intercalacao("output.txt");
+        if(argc == 5){
+            extSort ex{std::stoi(argv[1]),std::stoi(argv[2])};
+            ex.clfInterna(argv[3]);
+            ex.intercalacao(argv[4]);
+        }
     } catch(std::exception& e) {
         std::cout << "ERRO: " << e.what() << std::endl;
     }
